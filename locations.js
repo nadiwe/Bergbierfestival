6b7f26a8a975bbd2ed5de57a0b322ba8bd01da93
nadiwe <nadia.west@hispeed.ch>
2022-06-29 19:09:26 +0200

    loc tests

--- locations.js
+++ locations.js
@@ -35,37 +35,16 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoibndmd3NiIiwiYSI6ImNsNHNyaDBnbjBlenIzZGxhejg5e
             })
         );
         function geoFindMe() {
+            const successCallback = (position) =>{
+                console.log(position);
+            }
+            const errorCallback = (error) =>{
+                console.log(error);
+            }
+// navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
+const watchID = navigator.geolocation.watchPosition(successCallback,errorCallback);
+        }
 
-            const status = document.querySelector('#status');
-            const mapLink = document.querySelector('#map-link');
-          
-            mapLink.href = '';
-            mapLink.textContent = '';
-          
-            function success(position) {
-              const latitude  = position.coords.latitude;
-              const longitude = position.coords.longitude;
-          
-              status.textContent = '';
-              mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-              mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-            }
-          
-            function error() {
-              status.textContent = 'Unable to retrieve your location';
-            }
-          
-            if(!navigator.geolocation) {
-              status.textContent = 'Geolocation is not supported by your browser';
-            } else {
-              status.textContent = 'Locating…';
-              navigator.geolocation.getCurrentPosition(success, error);
-            }
-          
-          }
-          
-          document.querySelector('#find-me').addEventListener('click', geoFindMe);
-          
 
 //implement data - location points 
             map.on('load', () => {

73bb6fd03749284f0130b1795f27f9f261a6dcdb
nadiwe <nadia.west@hispeed.ch>
2022-06-30 11:57:56 +0200

    butto changed to div

--- locations.js
+++ locations.js
@@ -68,19 +68,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoibndmd3NiIiwiYSI6ImNsNHNyaDBnbjBlenIzZGxhejg5e
 
            
                
-          //user location 
-  /*  map.addControl(
-        new mapboxgl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true
-            },
-            // When active the map will receive updates to the device's location as it changes.
-            trackUserLocation: true,
-            // Draw an arrow next to the location dot to indicate which direction the device is heading.
-            showUserHeading: true
-        })
-    );
-*/
+ 
 
 
 
